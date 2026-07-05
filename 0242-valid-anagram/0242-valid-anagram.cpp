63b5f2c7678fdb6efe16a7594648a2d8e09530cc
Shruti Anand <106982471+ShrutiAnand03@users.noreply.github.com>
2026-07-05 16:53:18 +0530

    Time: 1 ms (55.1%), Space: 9.9 MB (11.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,19 +4,18 @@ public:
 
         if(s.size() != t.size()) return false;
         
-        unordered_map<char,int>mp_s;
-        unordered_map<char,int>mp_t;
+        unordered_map<char,int>mp;
 
-        for(int i=0;i<s.size();i++){
-            mp_s[s[i]]++;
+        for(auto c: s){  // o(n)
+            mp[c]++;
         }
 
-        for(int i=0;i<t.size();i++){
-            mp_t[t[i]]++;
+        for(auto c: t){  // o(n)
+            mp[c]--;
         }
 
-        for(int i=0;i<s.size();i++){
-            if(mp_s[s[i]] != mp_t[s[i]]) return false;
+        for(auto v: mp){
+            if(v.second !=0)return false;
         }
 
         return true;
